80e0ccb4b46d5b5794fc126212ff2721b40d6374
Maurilio Santana <cabelotaina@gmail.com>
2019-05-04 11:31:13 +0200

    added square validation

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,20 @@ function root(req, res, next) {
 }
 
 function isValidSquare(req, res, next) {
-  console.log(req.params.square);
-  next();
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  req.params.square = req.params.square.toLowerCase();
+
+  if (req.params.square.length === 2
+   && letters.includes(req.params.square[0])
+   && numbers.includes(req.params.square[1])) {
+    console.log('LOG: Valid Square');
+    next();
+  } else {
+    const error = new Error('Invalid Square');
+    next(error);
+  }
 }
 
 router.get('/knight/:square', isValidSquare, root);
