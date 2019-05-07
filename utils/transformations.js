d5a2d6e929afc08e59a31710066fbd4bb8c7a041
Maurilio Santana <cabelotaina@gmail.com>
2019-05-07 17:37:03 +0200

    remove unused utils lib

--- utils/transformations.js
+++
@@ -1,8 +0,0 @@
-const Square = require('models/square.js')
-
-module.exports = class Transform {
-  doTransformFor(squareString) {
-    let square = new Square(squareString[0], squareString[1]);
-    return square;
-  };
-};
