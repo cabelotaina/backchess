f012aa538bfb2745cc536ae97b21edc07e5bd113
Maurilio Santana <cabelotaina@gmail.com>
2019-05-07 17:36:10 +0200

    few updates on middleware validator

--- app.js
+++ app.js
@@ -18,19 +18,15 @@ function catch404Error(req, res, next) {
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.json({ success: false, message: err.message });
 }
 
-// catch 404 and forward to error handler
 app.use(catch404Error);
 
-// error handler
 app.use(errorHandler);
 
 module.exports = app;
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,15 +1,15 @@
-const validate = require('../utils/validations');
+const Square = require('../models/square');
 
 module.exports = {
   isValidSquare: function isValidSquare(req, res, next) {
 
     req.params.square = req.params.square.toLowerCase();
-    if (validate.isValidSquare(req.params.square)) {
+    let square = new Square(req.params.square[0], req.params.square[1]);
+    if (square.isValid()) {
       console.log('LOG: Valid Square');
       next();
     } else {
-      const error = new Error('This is not a acceptable Knight square position.');
-      next(error);
+      next(new Error('This is not a acceptable Knight square position.'));
     }
   },
 };
